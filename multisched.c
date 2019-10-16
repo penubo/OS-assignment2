5c5d41edd7335a32a445f8945531846abea9ab02
penubo <penubokim@gmail.com>
2019-10-16 19:51:17 +0900

    final

--- multisched.c
+++ multisched.c
@@ -7,29 +7,37 @@
 
 #define MSG(x...) fprintf (stderr, x)
 #define STRERROR  strerror (errno)
-#define COMMAND_LEN 256
 
-#define ID_LEN 2
-#define TIME_LEN 2
-#define PRIORITY_LEN 2
+/** constraints **/
+#define COMMAND_LEN 256						// maximum command length
 
-#define MAX_SERVICE_TIME 30
-#define MAX_PRIORITY 10
-#define MAX_ARRIVE_TIME 30
-#define MAX_PROCESS_ID 26*2*10
+#define ID_LEN 2									// id string length limit
+#define TIME_LEN 2								// time string length limit
+#define PRIORITY_LEN 2						// priority string length limit
 
-#define H_CPU_TIME_SLICE 6
-#define M_CPU_TIME_SLICE 4
+#define MAX_PRIORITY 10						// priority limit
+#define MAX_ARRIVE_TIME 30				// arrive time limit
+#define MAX_SERVICE_TIME 30				// service time limit
+#define MAX_PROCESS_ID 26*2*10		// all process id limit
+#define MIN_ARRIVE_TIME 0
+#define MIN_SERVICE_TIME 1
+#define MIN_PRIORITY 1
 
-#define DEBUG 0     // for debugging
+#define H_TIME_QUANTUM 6					// time quantum of H tasks
+#define M_TIME_QUANTUM 4					// time quantum of M tasks
 
-/* type definitions */
-typedef struct _Task Task;
+#define DEBUG 0
+
+
+/** declarations **/
+
+/* type declarations */
+typedef struct _Task Task;					
 typedef struct _Queue Queue;
 typedef struct _CPU CPU;
 typedef struct _GanttNode Node;
 typedef struct _GanttList GanttList;
-typedef enum _Type Type;
+typedef enum _Type Type;				
 
 /* parser related function declarations */
 static int check_valid_id(const char *);
@@ -41,7 +49,7 @@ static void append_task(Task *);
 
 /* queue related function declarations */
 static Queue *get_queue(Type);
-static int enqueue_task(Task *);
+static void enqueue_task(Task *);
 static Task *dequeue_task(Queue *);
 static bool is_empty(Queue *);
 static void init_queue(Queue *);
@@ -51,14 +59,12 @@ static void long_term_schedule();
 static void process();
 static void short_term_schedule();
 static void priority_interrupt_check();
-static void priority_interrupt();
 static void timeout_check();
 
 /* gantt related function declarations */
 static void add_gantt_node(Task *);
 static void record_to_gantt(char *);
 static void print_gantt();
-
 
 /* global variables declarations */
 static Task *tasks;           // list of tasks from the txt file.
@@ -132,14 +138,18 @@ struct _GanttList {
 
 /** function definitions **/
 
+/* make gantt node with a task */
 static void add_gantt_node(Task *task) {
-  Node *new_node = (Node *) malloc(sizeof(Node));
+
+  Node *new_node;
+	Node *n;
+
+  n = gantt_list.head;
+	new_node = (Node *) malloc(sizeof(Node));
   strcpy(new_node->id, task->id);
   new_node->task = task;
 
-  Node *n = gantt_list.head;
-
-  if (n == NULL) {
+  if (n == NULL) {								// if gantt node list is empty
     gantt_list.head = new_node;
     return;
   }
@@ -147,25 +157,26 @@ static void add_gantt_node(Task *task) {
   n->next = new_node;
 }
 
+/* record executed task information to corresponding gantt node */
 static void record_to_gantt(char *id) {
-  if (gantt_list.head == NULL) return;
 
-  Node *n = gantt_list.head;
+  if (gantt_list.head == NULL) return;
 
   for (Node *n = gantt_list.head; n != NULL; n = n->next) {
     if (!strcmp(n->id, id)) {
-      n->record[time] = true;
+      n->record[time] = true; 		// record that the task was executed at time
       break;
     }
   }
 
 }
 
+/* print gantt chart */
 static void print_gantt() {
 
-  if (gantt_list.head == NULL) return;
-
   Node *n = gantt_list.head;
+
+  if (n == NULL) return;
   
   while (n != NULL) {
     printf("%s ", n->id);
@@ -179,71 +190,26 @@ static void print_gantt() {
     printf("\n");
     n = n->next;
   }
-
-
 }
 
-// DEBUG
-void print_gantt_ids() {
-  Node *n = gantt_list.head;
-  if (n == NULL) {
-    MSG("gantt empty\n");
-  }
-  while (n != NULL) {
-    MSG("%s\n", n->id);
-    n = n->next;
-  }
-}
-
-// DEBUG
-static void print_tasks() {
-
-  Task *t = tasks;
-
-  if(!tasks) {
-    MSG("no tasks to print\n");
-  }
-
-  while (t != NULL) {
-    MSG ("%s %d-> ", t->id, t->priority);
-    t = t->next;
-  }
-  MSG("\n");
-}
-
-// DEBUG
-static void print_queue(Queue *q) {
-  Task *t = q->head;
-  if (t == NULL) {
-    MSG("queue is EMPTY\n");
-  } else {
-    MSG("%d queue : ", q->head->type);
-    while (t != NULL) {
-      MSG("%s -> ", t->id);
-      t = t->next;
-    }
-    MSG("\n");
-  }
-}
-
+/* check id is valid */
 static int check_valid_id(const char *str) {
 
-  size_t len;
-  int    i;
+  size_t len;						
 
   len = strlen (str);
-  if (len != ID_LEN)
+  if (len != ID_LEN)														// if ID length is invalid
     return -1;
 
-  if (!(isupper(str[0]) && isdigit(str[1]))) 
+  if (!(isupper(str[0]) && isdigit(str[1])))    // if ID is over ranged
     return -1;
 
   return 0;
 }
 
+/* look up task is existed */
+static Task *lookup_task (const char *id) {
 
-static Task *lookup_task (const char *id)
-{
   Task *task;
 
   for (task = tasks; task != NULL; task = task->next)
@@ -253,71 +219,74 @@ static Task *lookup_task (const char *id)
   return NULL;
 }
 
+/* check arrive time is valid */
 static int check_valid_arrive_time(const char *str) {
 
   size_t len;
   int val;
 
   len = strlen(str);
-  if (len > TIME_LEN)
+  if (len > TIME_LEN)											// if time length is invalid
     return -1;
 
   for (int i = 0; i < len; i++) {
-    if (!isdigit(str[i]))
+    if (!isdigit(str[i])) 								// if it is not digit values
       return -1;
   }
 
   val = atoi(str);
-  if (val < 0 || val > 30)
+  if (val < MIN_ARRIVE_TIME || val > MAX_ARRIVE_TIME)			// if it is over ranged
     return -1;
 
   return 0;
 }
 
+/* check service time is valid */
 static int check_valid_service_time(const char *str) {
 
   size_t len;
   int val;
 
   len = strlen(str);
-  if (len > TIME_LEN)
+  if (len > TIME_LEN)											// if its length is invalid
     return -1;
 
   for (int i = 0; i < len; i++) {
-    if (!isdigit(str[i]))
+    if (!isdigit(str[i]))									// if it is not digit value
       return -1;
   }
 
   val = atoi(str);
-  if (val < 1 || val > 30)
+  if (val < MIN_SERVICE_TIME || val > MAX_SERVICE_TIME)		// if it is over ranged
     return -1;
 
   return 0;
 }
 
+/* check priority is valid */
 static int check_valid_priority(const char *str) {
 
   size_t len;
   int val;
 
   len = strlen(str);
-  if (len > PRIORITY_LEN)
+  if (len > PRIORITY_LEN)									// if its length is invalid
     return -1;
 
-  for (int i = 0; i < len; i++) {
+  for (int i = 0; i < len; i++) {					// if it is not digit value
     if (!isdigit(str[i]))
       return -1;
   }
 
   val = atoi(str);
-  if (val < 1 || val > 10)
+  if (val < MIN_PRIORITY || val > MAX_PRIORITY)   // if it is over ranged
     return -1;
 
   return 0;
 }
 
-static char * strstrip (char *str)
-{
+static char * strstrip (char *str) {
+
   char  *start;
   size_t len;
 
@@ -335,6 +304,7 @@ static char * strstrip (char *str)
   return str;
 }
 
+/* append task to tasks list */
 static void append_task(Task *task) {
 
   Task *new_task;
@@ -367,6 +337,7 @@ static void append_task(Task *task) {
 
 }
 
+/* parsing data file */
 static int read_config(const char* filename) {
 
   FILE *fp;
@@ -487,6 +458,7 @@ static int read_config(const char* filename) {
       MSG ("id:%s type:%d arrive-time:%d service-time:%d priority:%d\n",
           task.id, task.type, task.arrive_time, task.service_time, task.priority);
 
+		/* append task */
     append_task(&task);
 
     continue;
@@ -501,6 +473,7 @@ invalid_line:
 
 }
 
+/* get queue */
 static Queue *get_queue(Type type) {
   switch(type) {
     case H:
@@ -515,73 +488,71 @@ static Queue *get_queue(Type type) {
   }
 }
 
-static int enqueue_task(Task *new_task) {
+/* enqueue task to corresponding queue */
+static void enqueue_task(Task *new_task) {
 
   Task *t;
+	Type task_type;
+	Queue *q;
 
   if (!new_task) {
     MSG("enqueue_task error no task is given\n");
-    return -1;
+    return ;
   }
 
-  Type task_type = new_task->type;
-  Queue *q = get_queue(task_type);
+  task_type = new_task->type;
+  q = get_queue(task_type);					// get queue from task's type
   t = q->head;
 
-  if (is_empty(q)) {
+  if (is_empty(q)) {								// when queue is empty
     q->head = q->tail = new_task;
   } else if (task_type == H) {
 
-    if (new_task->priority < q->head->priority) {
+		// enqueue by its priority
+    if (new_task->priority < q->head->priority) { 		
       new_task->next = q->head;
       q->head = new_task;
     } else { 
       while (t->next != NULL 
-          && new_task->priority >= t->next->priority) 
-        t = t->next;
-      if (t->next == NULL) {
-        t->next = new_task;
-        q->tail = new_task;
-      } else {
-        new_task->next = t->next;
-        t->next = new_task;
-      }
+          && new_task->priority >= t->next->priority) t = t->next;
+
+			new_task->next = t->next;
+			t->next = new_task;
     }
 
   } else if (task_type == M) {
 
-    if (new_task->remaining_time < q->head->remaining_time) {
+		// enqueue by its remaining time
+    if (new_task->remaining_time < q->head->remaining_time) {  
       new_task->next = q->head;
       q->head = new_task;
     } else {
       while (t->next != NULL 
-             && new_task->remaining_time >= t->next->remaining_time) 
-        t = t->next;
-        if (t->next == NULL) {
-          t->next = new_task;
-          q->tail = new_task;
-        } else {
-          new_task->next = t->next;
-          t->next = new_task;
-        }
+             && new_task->remaining_time >= t->next->remaining_time) t = t->next;
+
+				new_task->next = t->next;
+				t->next = new_task;
     }
 
   } else if (task_type == L) {
 
+		// FIFO implementation
     q->tail->next = new_task;
     q->tail = new_task;
   }
-
 }
 
+/* dequeue a task from given queue */
 static Task *dequeue_task(Queue *q) {
+
+  Task *t;
 
   if (is_empty(q)) {
     MSG ("no element to dequeue\n");
     return NULL;
   }
 
-  Task *t = q->head;
+	t = q->head;
 
   if (q->head == q->tail) {
     q->head = NULL;
@@ -596,10 +567,12 @@ static Task *dequeue_task(Queue *q) {
 
 }
 
+/* check whether queue is empty */
 static bool is_empty(Queue *q) {
   return (q->tail == NULL);
 }
 
+/* init queue */
 static void init_queue(Queue *q) {
   q->head = NULL;
   q->tail = NULL;
@@ -627,86 +600,92 @@ static void long_term_schedule() {
   }
 }
 
+/* process task in cpu */
 static void process() {
+
   if (cpu->task != NULL) {
-    record_to_gantt(cpu->task->id);
-    cpu->task->remaining_time--;
-    if (cpu->task->remaining_time == 0) {
+
+    record_to_gantt(cpu->task->id);						// record to gantt node
+    cpu->task->remaining_time--;							// update remaining time of the task
+
+    if (cpu->task->remaining_time == 0) {			// when task is done
+
       if (DEBUG) MSG ("task %s is done\n", cpu->task->id);
-      cpu->task->complete_time = time + 1;
-      cpu->task = NULL;
+
+      cpu->task->complete_time = time + 1;		// record complete time
+      cpu->task = NULL;												// we add 1 because time is not ticking yet
     }
-    cpu->timeout--;
+    cpu->timeout--;			// update timeout value
   }
 }
 
+/* short-term-scheduling function */
 static void short_term_schedule() {
 
-  if (cpu->task != NULL) return;
-  
   Task *t;
 
-  if (cpu->task_type == H) { // it's time for H task
+  if (cpu->task != NULL) return;
+
+  if (cpu->task_type == H) { 						// it's time for H task
+    if (!is_empty(H_queue)) {						// if there is H task to be able to run
+      t = dequeue_task(H_queue);
+      cpu->task = t;
+    } else if (!is_empty(M_queue)) { 		// if there is M task to be able to run and 
+      t = dequeue_task(M_queue);				// no H task to be able to run 
+      cpu->task = t;
+      cpu->task_type = M;								// reset time quantum to M
+      cpu->timeout = M_TIME_QUANTUM;
+    } else if (!is_empty(L_queue)) {		// if no H, M task to be able to run
+      t = dequeue_task(L_queue);
+      cpu->task = t;
+      cpu->task_type = L;
+      cpu->timeout = -1;								// no timeout for L task
+    }
+
+  } else if (cpu->task_type == M) { 		// it's time for M task 
+    if (!is_empty(M_queue)) {						// if there is M task to be able to run
+      t = dequeue_task(M_queue);
+      cpu->task = t;
+    } else if (!is_empty(H_queue)) {		// if there is H task to be able to run and
+      t = dequeue_task(H_queue);				// no M task to be able to run
+      cpu->task = t;
+      cpu->task_type = H;								// reset time quantum to H
+      cpu->timeout = H_TIME_QUANTUM;
+    } else if (!is_empty(L_queue)) {		// if no H, M task to be able to run
+      t = dequeue_task(L_queue);
+      cpu->task = t;
+      cpu->task_type = L;
+      cpu->timeout = -1;								// no time out for L task
+    }
+
+  } else { 															// if L was running before or no task was run yet
     if (!is_empty(H_queue)) {
-      t = dequeue_task(H_queue);
+      t = dequeue_task(H_queue);				// if there is H task to be able to run
       cpu->task = t;
-    } else if (!is_empty(M_queue)) {
+      cpu->task_type = H;								// reset time quantum
+      cpu->timeout = H_TIME_QUANTUM;
+    } else if (!is_empty(M_queue)) {		// if there is M task to be able to run
       t = dequeue_task(M_queue);
       cpu->task = t;
-      cpu->task_type = M;
-      cpu->timeout = M_CPU_TIME_SLICE;
-    } else if (!is_empty(L_queue)) {
+      cpu->task_type = M;								// reset time quantum
+      cpu->timeout = M_TIME_QUANTUM;
+    } else if (!is_empty(L_queue)) {		// if no H, M task to be able to run
       t = dequeue_task(L_queue);
       cpu->task = t;
       cpu->task_type = L;
       cpu->timeout = -1;
-    }
-
-  } else if (cpu->task_type == M) { // it's time for M task 
-    if (!is_empty(M_queue)) {
-      t = dequeue_task(M_queue);
-      cpu->task = t;
-    } else if (!is_empty(H_queue)) {
-      t = dequeue_task(H_queue);
-      cpu->task = t;
-      cpu->task_type = H;
-      cpu->timeout = H_CPU_TIME_SLICE;
-    } else if (!is_empty(L_queue)) {
-      t = dequeue_task(L_queue);
-      cpu->task = t;
-      cpu->task_type = L;
-      cpu->timeout = -1;
-    }
-
-  } else { // if L was running before or no task was run yet
-    if (!is_empty(H_queue)) {
-      t = dequeue_task(H_queue);
-      cpu->task = t;
-      cpu->task_type = H;
-      cpu->timeout = H_CPU_TIME_SLICE;
-    } else if (!is_empty(M_queue)) {
-      t = dequeue_task(M_queue);
-      cpu->task = t;
-      cpu->task_type = M;
-      cpu->timeout = M_CPU_TIME_SLICE;
-    } else if (!is_empty(L_queue)) {
-      t = dequeue_task(L_queue);
-      cpu->task = t;
-      cpu->task_type = L;
-      cpu->timeout = -1;
-    } else {
-
-    }
+    } else {}
   }
-
-
 }
 
+/* handle priority interrupt if there is */
 static void priority_interrupt_check() {
 
   if (cpu->task == NULL) return;
 
   // if M is running then no preemption occur
+
+	// when H is running and more higher priority appear then interrupt
   if (cpu->task_type == H) 
   {
     if (!is_empty(H_queue)) {
@@ -718,6 +697,7 @@ static void priority_interrupt_check() {
       }
     }
   } 
+	// when L is running and either H, M tasks appear then interrupt
   else if (cpu->task_type == L) 
   {
     if (!is_empty(H_queue)) {
@@ -725,7 +705,7 @@ static void priority_interrupt_check() {
       Task *new_task = dequeue_task(H_queue);
       cpu->task = new_task;
       // update time quantum to H
-      cpu->timeout = H_CPU_TIME_SLICE;
+      cpu->timeout = H_TIME_QUANTUM;
       cpu->task_type = H;
       // preempted L task must handle first later
       preempted_task->next = L_queue->head->next;
@@ -735,7 +715,7 @@ static void priority_interrupt_check() {
       Task *new_task = dequeue_task(M_queue);
       cpu->task = new_task;
       // update time quantum to M
-      cpu->timeout = M_CPU_TIME_SLICE;
+      cpu->timeout = M_TIME_QUANTUM;
       cpu->task_type = M;
       // preempted L task must handle first later
       preempted_task->next = L_queue->head->next;
@@ -744,23 +724,30 @@ static void priority_interrupt_check() {
   }
 }
 
+/* handle timeout if there is */
 static void timeout_check() {
+
+	Task *preempted_task;
+
   if (cpu->task == NULL) return;
 
+	// switcing H to M task
   if (cpu->task_type == H && cpu->timeout == 0) {
 
     cpu->task_type = M;
-    cpu->timeout = M_CPU_TIME_SLICE;
+    cpu->timeout = M_TIME_QUANTUM;
     // remove task
-    Task *preempted_task = cpu->task;
+    preempted_task = cpu->task;
     enqueue_task(preempted_task);
     cpu->task = NULL;
+
+	// switching M to H
   } else if (cpu->task_type == M && cpu->timeout == 0) {
 
     cpu->task_type = H;
-    cpu->timeout = H_CPU_TIME_SLICE;
+    cpu->timeout = H_TIME_QUANTUM;
     // remove task
-    Task *preempted_task = cpu->task;
+    preempted_task = cpu->task;
     enqueue_task(preempted_task);
     cpu->task = NULL;
   }
@@ -768,15 +755,16 @@ static void timeout_check() {
   return;
 }
 
+/* calculate average turnaround time */
 static double get_average_turn_around_time() {
 
   int size = 0;
   int total_turn_around_time = 0;
 
-  Node *n = gantt_list.head;
-  if (n == NULL) return 0.0;
+  if (gantt_list.head == NULL) return 0.0;
 
   for (Node *n = gantt_list.head; n != NULL; n = n->next) {
+		// turnaround time = complete time - arrive time
     n->turn_around_time = n->task->complete_time - n->task->arrive_time;
     total_turn_around_time += n->turn_around_time;
     size++;
@@ -786,18 +774,20 @@ static double get_average_turn_around_time() {
   return ((double) total_turn_around_time) / size; 
 }
 
+/* calculate average waiting time */
 static double get_average_waiting_time() {
 
   int size = 0;
   int total_waiting_time = 0;
 
   for (Node *n = gantt_list.head; n != NULL; n = n->next) {
+		// waiting time = turnaround time - arrive time
     n->waiting_time = n->turn_around_time - n->task->service_time;
     total_waiting_time += n->waiting_time;
     size++;
   }
 
-  return (double) total_waiting_time / size;
+  return ((double) total_waiting_time) / size;
 }
 
 int main(int argc, char **argv) {
@@ -837,12 +827,6 @@ int main(int argc, char **argv) {
 
     /* long-term scheduling */
     long_term_schedule();
-
-    if (DEBUG) {
-      print_queue(H_queue);
-      print_queue(M_queue);
-      print_queue(L_queue);
-    }
 
     /* short_term_scheduling */
     if (cpu->task == NULL) {
